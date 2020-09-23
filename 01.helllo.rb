0790a5214bfd9f85113abce7685e8f20f3a7a4ba
phuongnguyen <vectorgroup@vectors-MacBook-Pro.local>
2020-09-23 13:31:44 +0700

    add hello file

---
+++ 01.helllo.rb
@@ -0,0 +1,24 @@
+=begin
+    
+Bai hoc dau tin ve ruby 
+    
+=end
+class Person 
+
+    def initialize(name)
+        @name=name
+    end
+    # dinh nghia getter doc
+    def name 
+        @name
+    end
+    #dinh nghia better de updtae 
+    def name=(name)
+
+        @name=name
+    end
+end
+
+p=Person.new("ihone")
+p.name="laptop"
+puts p.name
