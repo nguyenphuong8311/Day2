eaf24243d89ee0f92b1ade21202af3d16166a397
phuongnguyen <vectorgroup@vectors-MacBook-Pro.local>
2020-09-25 17:28:45 +0900

    update 01.helllo file

--- 01.helllo.rb
+++ 01.helllo.rb
@@ -3,37 +3,14 @@
 Bai hoc dau tin ve ruby 
     
 =end
-class Person 
+xinchao = -> (name) {
+    puts "hellov #{name}"
+} 
+xinchao.call("XML")
 
-    def initialize(name)
-        @name=name
-    end
-    # dinh nghia getter doc
-    def name 
-        @name
-    end
-    #dinh nghia better de updtae 
-    def name=(name)
+xinchao2 = -> (name) {
+    puts "Xin chào #{name}"
+}
 
-        @name=name
-    end
-end
-
-p=Person.new("ihone")
-p.name="laptop"
-puts p.name
-
-a=10
-puts a
-print (" nhap gia tri moi cua a")
-a=gets
-puts ("in ra gia tri moi cua a:#{a}")
-
-a=100
-b=200
-if (a>b)
-    puts "#{a}lon hon #{b}"
-else puts "#{a} nho hon #{b}"
-end
-
-
+xinchao2.call("XTL")
+# Xin chào XTL
